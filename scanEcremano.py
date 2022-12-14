2c0001ec5d07578c9b5097a8307efd9ccd0345f1
Flavio Pace <flavio.pace@gmail.com>
2022-12-14 22:32:51 +0100

    add more

--- scanEcremano.py
+++ scanEcremano.py
@@ -3,15 +3,19 @@ from bs4 import BeautifulSoup as bs
 from PyPDF2 import PdfReader
 import re
 import io
+import os
+import json
 from google.cloud import vision, storage
 
+SCAN_PAGE_MAX = 30
 
 baseURL = 'http://www.e-cremano.it/cda/detail.jsp?otype=1027&id=102469&type=Permesso%20di%20Costruire&archive=true&page='
 baseDownload = 'http://www.e-cremano.it/'
 
-def parseHTML():
+def parseHTML(storeFile=False):
 
-    for page in range(1, 30):
+    pratiche = []
+    for page in range(1, SCAN_PAGE_MAX):
         myurl = baseURL + str(page)
         print("parsing page {}: {}".format(page, myurl))
 
@@ -22,29 +26,19 @@ def parseHTML():
             print(div.findAll('h1')[1])
         for link in soup.findAll('a', href=True):
             if "PAP"  in link.text:
-                localname = '/Users/unicondor/Downloads/' + link['href'].split('/')[6]
+                fileIn = link['href'].split('/')[6]
+                localname = '/Users/flaviopace/Downloads/' + fileIn
                 pdflink = baseDownload + link['href']
                 print(pdflink)
                 #save locally
-                response = requests.get(pdflink)
-                with open(localname, 'wb') as f:
-                    f.write(response.content)
+                pratiche.append(fileIn)
+                if not isBlobsAvailable('pdf-ecremano', fileIn):
+                    response = requests.get(pdflink)
+                    with open(localname, 'wb') as f:
+                        f.write(response.content)
+                    uploadBlob('pdf-ecremano', localname, fileIn)
 
-                #ret = upload_to_bucket(link['href'].split('/')[6], localname, "pdf-ecremano")
-                #print(ret)
-
-                # try to read PDF, but they are images
-                # response = requests.get(pdflink)
-                # with io.BytesIO(response.content) as open_pdf_file:
-                #     read_pdf = PdfReader(open_pdf_file)
-                #     num_pages = read_pdf.getNumPages()
-                #     #print(num_pages)
-                #     PageObj = read_pdf.getPage(0)
-                #     pagetxt = PageObj.extractText()
-                #     m = re.search("Criscuolo", pagetxt)
-                #     if m:
-                #         print(m.group(0))
-
+    return pratiche
 
 def upload_to_bucket(blob_name, path_to_file, bucket_name):
     """ Upload data to a bucket"""
@@ -79,7 +73,7 @@ def convertPDF():
         vertices = ['(%s,%s)' % (v.x, v.y) for v in text.bounding_poly.vertices]
         print('bounds:', ",".join(vertices))
 
-def async_detect_document(gcs_source_uri, gcs_destination_uri):
+def async_detect_document(gcs_source_uri, gcs_destination_uri, localfile):
     """OCR with PDF/TIFF as source files on GCS"""
     import json
     import re
@@ -148,12 +142,95 @@ def async_detect_document(gcs_source_uri, gcs_destination_uri):
     # The response contains more information:
     # annotation/pages/blocks/paragraphs/words/symbols
     # including confidence scores and bounding boxes
-    print('Full text:\n')
-    print(annotation['text'])
+    with open(localfile, 'wb') as f:
+        f.write('Full text:\n'.encode())
+        f.write(annotation['text'].encode())
+
+def jsonToTxt(directory):
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f) and "ALBO" in f:
+            print(f)
+            # Opening JSON file
+            f = open(f)
+
+            # returns JSON object as
+            # a dictionary
+            data = json.load(f)
+            annotation = data['responses'][0]['fullTextAnnotation']
+            text = annotation['text']
+            #localfile = f.
+            outfile = filename + '.txt'
+            print(outfile)
+            with open(outfile, 'wb') as out:
+                out.write('Full text:\n'.encode())
+                out.write(annotation['text'].encode())
+            # match = re.match(r'sig(.*?), nat', text)
+            # bucket_name = match.group(1)
+            # prefix = match.group(2)
+
+            # Iterating through the json
+            # list
+            # for i in data['text']:
+            #     print(i)
+
+            # Closing file
+            f.close()
+
+def getInfo(directory):
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            print(filename)
+            f = open(f)
+            text = f.read()
+            match = re.match(r'(S|s)ig(.*)', text)
+            if match:
+                name = match.group(1)
+                prefix = match.group(2)
+            f.close()
+
+
+def isBlobsAvailable(b_name, name):
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_or_name=b_name)
+
+    storedPdf = []
+    for bl in blobs:
+        storedPdf.append(bl.name)
+
+    if name in storedPdf:
+        print("{} already available".format(name))
+        return True
+    else:
+        print("{} new".format(name))
+        return False
+
+
+def uploadBlob(b_name, name, blob_name):
+
+    client = storage.Client()
+    bucket = client.get_bucket(b_name)
+    blob = bucket.blob(blob_name)
+    with open(name, "rb") as my_file:
+        blob.upload_from_file(my_file)
 
 if __name__ == "__main__":
-    #parseHTML()
-    convertPDF()
-    async_detect_document('gs://pdf-ecremano/ALBOPRETORIO_FILE_431231.pdf', 'gs://pdf-ecremano/ALBOPRETORIO_FILE_435930.txt')
+    pr = parseHTML()
+
+    #convertPDF()
+    # for currpr in pr:
+    #     print("AI ML Processing PDF {}".format(currpr))
+    #     googlepath = 'gs://pdf-ecremano/' + currpr
+    #     async_detect_document(googlepath, googlepath + '.txt', currpr.replace('pdf', 'txt'))
+    #jsonToTxt('/Users/flaviopace/Documents/repos/e-cremano/e-cremano')
+    #getInfo('/Users/flaviopace/Documents/repos/e-cremano/e-cremano-txt')
 
 
